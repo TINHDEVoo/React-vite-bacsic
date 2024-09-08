355231b61ce5f8ed649841299c0d32c5ab611b61
TINH DEV <tinhdev@gmail.com>
2024-09-08 14:34:00 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,12 +3,21 @@ const TodoNew = (props) => {
     const { addNewTodo } = props;
 
     // addNewTodo("eric")
+
+    const hanleClick = () => {
+        alert("Click me")
+    }
+    const hanleChange = (name) => {
+        console.log(">> check hanleOnchange ", name)
+    }
     return (
         <div className="todolist">
             <label>Todo list</label>
             <div>
-                <input type="text" />
-                <button>Add</button>
+                <input type="text"
+                    onChange={(event) => { hanleChange(event.target.value) }} />
+                <button style={{ cursor: "pointer" }}
+                    onClick={hanleClick}>Add</button>
             </div>
         </div>
     );
