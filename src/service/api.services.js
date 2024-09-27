40625cfc8377e31f13e13c06854f1d2ed3928ff4
TINH DEV <tinhdev@gmail.com>
2024-09-27 21:46:10 +0700

    video 73

--- src/service/api.services.js
+++ src/service/api.services.js
@@ -10,7 +10,14 @@ const createUserApi = (fullName, email, password, phone) => {
     }
     return axios.post(url_http, data)
 }
+const updateUserApi = () => {
 
+}
+
+const fetchAllUserAPI = () => {
+    const url_http = "/api/v1/user";
+    return axios.get(url_http)
+}
 export {
-    createUserApi
+    createUserApi, updateUserApi, fetchAllUserAPI
 }
